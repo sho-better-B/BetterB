d667081d3ef45b1f05dfc58c2c4a31e7b55edb79
HmidiHoussem <hmidi.houssem@gmail.com>
2022-07-12 18:27:16 +0100

    test

--- server/dataBase/user.d.ts
+++ server/dataBase/user.d.ts
@@ -1,32 +1,27 @@
-import { Schema , model ,connect } from "mongoose";
+import { Schema, model, connect } from "mongoose";
 
 // 1. Create an interface representing a document in MongoDB.
 
 interface user {
-    name: string;
-img: string;
-email: string;
-weight: number;
-height: number;
-imc: number;
+  name: string;
+  img: string;
+  // email: string;
+  weight: number;
+  height: number;
+  imc: number;
 }
-// 2. Create schema unsing typescript 
-const userSchema = new Schema<user>(
-    {
-        name: String,
-        img: String,
-        email: String,
-        weight: Number,
-        height: Number,
-        imc: Number,
+// 2. Create schema unsing typescript
+const userSchema = new Schema<user>({
+  name: String,
+  img: String,
+  email: String,
+  weight: Number,
+  height: Number,
+  imc: Number,
+});
 
-    }
-);
+// 3. create user model
 
-// 3. create user model 
-
-const userProfile = model<user>('users',userSchema);
+const userProfile = model<user>("users", userSchema);
 
 module.exports = userProfile;
-
-

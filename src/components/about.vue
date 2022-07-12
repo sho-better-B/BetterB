80d585a37eaec41476b90d6bde53072f4e6747a9
HmidiHoussem <hmidi.houssem@gmail.com>
2022-07-12 14:11:58 +0100

    components test

---
+++ src/components/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class About extends Vue {
+  @Prop() private message!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <About message="this is test for the about page " />
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import About from "@/components/about.vue";
+
+@Component({
+  components: {
+    About,
+  },
+})
+export default class AboutView extends Vue {}
+</script>

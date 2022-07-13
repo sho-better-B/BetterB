947f4a9f47d83fa35c6fff3af01b557406ff0338
HmidiHoussem <hmidi.houssem@gmail.com>
2022-07-13 11:37:34 +0100

    get user done

--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,27 @@
-import  express  from "express";
-import {json} from 'body-parser'
+import express from "express";
+import { json } from "body-parser";
 import mongoose from "mongoose";
-const app = express()
-const Port= 2000 
- //import diet schema and router
- import { dietRouter } from "./Router/diets";
- import {ReceipeModel} from "./dataBase/diet"
+const app = express();
+const Port = 2000;
+//import diet schema and router
+import { dietRouter } from "./Router/diets";
+import { ReceipeModel } from "./dataBase/diet";
+import { userRouter } from "./Router/user";
 
 app.use(json());
-app.use(dietRouter)
+app.use(dietRouter);
 
-
-app.get("/", (req, res) =>{
-    res.send("te5dem")
-})
+app.get("/", (req, res) => {
+  res.send("te5dem");
+});
+// user
+app.use(userRouter);
 
 //connect to database
-mongoose.connect('mongodb://localhost:27017/betterB',()=>{
-   console.log("Connected to database...")
-})
+mongoose.connect("mongodb://localhost:27017/betterB", () => {
+  console.log("Connected to database...");
+});
 
-app.listen(Port,() => {
-    console.log("server connected successfully on port " +Port);
-})
+app.listen(Port, () => {
+  console.log("server connected successfully on port " + Port);
+});

34c5482ac4172034b8467f857cd9e5c3dc682b57
HmidiHoussem <hmidi.houssem@gmail.com>
2022-07-13 19:24:25 +0100

    merge

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,10 @@ import express from "express";
 import { json } from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-const app = express()
-const Port= 2000 
- //import diet schema and router
- import { dietRouter } from "./Router/diets";
-app.use(cors());
+const app = express();
+const Port = 2000;
+
+app.use(cors);
 
 app.use(json());
 

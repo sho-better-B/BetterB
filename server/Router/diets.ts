56a2d31bcc072c273e0a9ca4bdf5f6837e876681
omarzouaoui <zouaouiomar80@gmail.com>
2022-07-13 11:53:09 +0100

    soon

--- server/Router/diets.ts
+++ server/Router/diets.ts
@@ -12,23 +12,5 @@ router.get('/api/diet', async (req: Request, res: Response) => {
   return res.status(200).send(recipes)
 })
 
-// const addRecipes = async (req: Request, res: Response)  => {
-//     try {
-//       const body = req.body.nameReceipe
-  
-//       const  Ireecipe= new ReceipeModel({
-//       description: body.description,
-//         status: body.status,
-//       })
-  
-//       const newTodo: ITodo = await todo.save()
-//       const allTodos: ITodo[] = await Todo.find()
-  
-//       res
-//         .status(201)
-//         .json({ message: "Todo added", todo: newTodo, todos: allTodos })
-//     } catch (error) {
-//       throw error
-//     }
-//   }
+
 export {router as dietRouter}
--- server/index.ts
+++ server/index.ts
@@ -3,20 +3,40 @@ import {json} from 'body-parser'
 import mongoose from "mongoose";
 const app = express()
 const Port= 2000 
+
+app.use(json());
+
  //import diet schema and router
  import { dietRouter } from "./Router/diets";
  import {ReceipeModel} from "./dataBase/diet"
 
-app.use(json());
+ //diet
 app.use(dietRouter)
 
 
 app.get("/", (req, res) =>{
     res.send("te5dem")
 })
+//workout
+import { workoutRouter } from "./Router/workout";
+import {WorkoutModel} from "./dataBase/workout"
+
+app.use(workoutRouter)
+
+app.get('/workout',(req,res)=>{
+    res.send('mriigla')
+})
+app.post("/workout/post",(req,res)=>{
+    res.send('tzeed')
+})
+
+
+//sign
+
 
 //connect to database
-mongoose.connect('mongodb://localhost:27017/betterB',()=>{
+mongoose.connect('mongodb://localhost:27017/betterB',
+()=>{
    console.log("Connected to database...")
 })
 

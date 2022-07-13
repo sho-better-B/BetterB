7a6637644213299a17e9ba3b729f05bbcce649ca
HmidiHoussem <hmidi.houssem@gmail.com>
2022-07-13 12:12:07 +0100

    merge

--- server/index.ts
+++ server/index.ts
@@ -2,51 +2,35 @@ import express from "express";
 import { json } from "body-parser";
 import mongoose from "mongoose";
 
-const app = express()
-const Port= 2000 
+const app = express();
+const Port = 2000;
 
 app.use(json());
 
- //import diet schema and router
- import { dietRouter } from "./Router/diets";
- import {ReceipeModel} from "./dataBase/diet"
+//diet
+//import diet schema and router
+import { dietRouter } from "./Router/diets";
 
- //diet
-app.use(dietRouter)
+app.use(dietRouter);
 
-
-app.get("/", (req, res) =>{
-    res.send("te5dem")
-})
 //workout
 import { workoutRouter } from "./Router/workout";
-import {WorkoutModel} from "./dataBase/workout"
 
-app.use(workoutRouter)
-
-app.get('/workout',(req,res)=>{
-    res.send('mriigla')
-})
-app.post("/workout/post",(req,res)=>{
-    res.send('tzeed')
-})
-
+app.use(workoutRouter);
 
 // user
+import { userRouter } from "./Router/user";
+
 app.use(userRouter);
 
-
-
-
 //connect to database
-mongoose.connect('mongodb://localhost:27017/betterB',
-()=>{
-   console.log("Connected to database...")
-})
+app.get("/", (req, res) => {
+  res.send("te5dem");
+});
 
-
-
-
+mongoose.connect("mongodb://localhost:27017/betterB", () => {
+  console.log("Connected to database...");
+});
 
 app.listen(Port, () => {
   console.log("server connected successfully on port " + Port);

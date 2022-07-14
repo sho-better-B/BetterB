21779533ad0c7cc96c04dddb29a7773052b3ee7f
HmidiHoussem <hmidi.houssem@gmail.com>
2022-07-14 10:20:56 +0100

    test

--- src/components/diet.vue
+++ src/components/diet.vue
@@ -1,10 +1,10 @@
 <template>
- <v-layout>
+  <v-layout>
     <v-flex>
       <v-list>
         <v-list-item :key="index" v-for="(recipe, index) in recipes">
-         <div>
-          {{recipe.nameReceipe}}
+          <div>
+            {{ recipe.nameReceipe }}
           </div>
         </v-list-item>
       </v-list>
@@ -14,18 +14,15 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import {recipesApi }from "@/components/api"
-import Diets from "@/components/interface"
+import { recipesApi } from "@/components/api/dietApi";
+import Diets from "@/components/interface/dietInterface";
 @Component
 export default class Diet extends Vue {
+  recipes: Diets[] = [];
 
- recipes: Diets[] = [];
-
- async mounted(): Promise<void> {
+  async mounted(): Promise<void> {
     this.recipes = await recipesApi.getAllrecipes();
-   
   }
-
 }
 </script>
 

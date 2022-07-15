0c18d843a524c9e8f54745eedf4328aca518c3e5
sameh-dh <samehdh44@gmail.com>
2022-07-15 14:31:54 +0100

    more css is done

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
   <div id="app">
     <nav class="nav">
       <h1>Better-B</h1>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/training">Training</router-link>|
-      <router-link to="/diet">Diet</router-link>|
-      <router-link to="/blogs">Blogs</router-link> |
-      <router-link to="/signIn">sign In</router-link>
+      <router-link to="/" ><h1> Home</h1></router-link> 
+      <router-link to="/about" ><h1>About</h1></router-link> 
+      <router-link to="/training" ><h1>Training</h1></router-link>
+      <router-link to="/diet" ><h1>Diet</h1></router-link>
+      <router-link to="/blogs" ><h1>Blogs</h1></router-link> 
+      <router-link to="/signIn" ><h1>sign In</h1></router-link>
     </nav>
     <router-view />
   </div>
@@ -20,8 +20,11 @@ export default defineComponent({
 </script>
 
 <style>
-
-#app {
+body{
+  background-color: #ADB3BC;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
+}
+ #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -29,17 +32,50 @@ export default defineComponent({
   color: #384552;
 }
 
-nav {
-  padding: 30px;
-  background-color: rgb(18, 129, 114);
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+/* nav bar css */
+nav h1{
+  position: relative;
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+ float:left;
+  color: #f2f2f2;
+  padding-left: 10px;
+  text-decoration: none;
+  font-size: 20px;
+  padding-right: 100px;
+  text-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+  height: 70px;
+  padding-top: 30px;
+  margin-top: 0;
+ 
+
+ }
+ nav {
+  position: relative;
+
+margin-bottom: 40px;
+  overflow: hidden;
+  background-image :linear-gradient(to top, #211b1c 0%, #150f10 52%, #0a0b0e 100%); 
+   width: 100%;
+  height: 90px;
+  box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
+ }
+ nav h1:hover{
+  position: relative;
+
+ text-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
+      cursor: pointer;
+
+ }
+ 
+
+ nav :hover{
+  position: relative;
+
+ text-align: center;
+TEXT-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;
+ 
+
+
+ }
 </style>
